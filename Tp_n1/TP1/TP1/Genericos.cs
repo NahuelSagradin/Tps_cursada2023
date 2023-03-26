f869bfad673e42b45120358aca40dcf71c0b70f0
nahuel.sagradin <n.sagradin@gmail.com>
2023-03-26 09:28:37 -0300

    Comienzo del TP1

---
+++ Tp_n1/TP1/TP1/Genericos.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1
+{
+    public class Genericos<T>
+    {
+        // La programacion generica nos facilita la reutilizacion de codigo,
+        // consiste en crear una clase comodin que me permita crear objetos
+        // de distintos tipos (int, String, etc). Por tal motivo nos brinda
+        // la opcion de trabajar con un codigo mas sencillo a la vez que la
+        // comprobacion de errores se realiza en tiempo de compilacion!
+
+        // Esto lo conseguimos al momento de instanciar dicha clase, a
+        // continuacion del nombre de la clase entre parentesis angulares
+        // colocamos el tipo de objeto que vamos a manejar como parametro.
+
+        // EjemploGenerico <Tipo> xObjeto = new EjemploGenerico<Tipo>();
+
+        private T[] datosElemento;
+        private int i = 0;
+
+        public Genericos(int indice) {
+            datosElemento = new T[indice];
+        }
+
+        public void agregar(T obj) {
+            datosElemento[i] = obj;
+            i++;
+        }
+
+        public T getElemento(int i)
+        {
+            return datosElemento[i];
+        }
+
+        public T[] getDatosElemento() => datosElemento;
+
+
+    }
+}
---
+++ Tp_n1/TP1/TP1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using TP1;
+
+Genericos<String> cadena = new Genericos<string>(4);
+
+cadena.agregar("Carlos");
+cadena.agregar("Jorge");
+cadena.agregar("Diego");
+cadena.agregar("Braian");
+
+for (int i = 0; i < cadena.getDatosElemento().Length; i++)
+{
+    Console.WriteLine(cadena.getElemento(i));
+}
+
